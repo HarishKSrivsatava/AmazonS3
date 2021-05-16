891f5d032887d295bcb23b79eaa416ad9b44e8df
Harish Kumar <harishkumar9758@gmail.com>
2021-05-16 08:58:26 +0530

    Amazon-S3-first-commit

---
+++ AmazonS3/src/main/java/com/harish/aws/amazon/s3/AmazonS3/App.java
@@ -0,0 +1,13 @@
+package com.harish.aws.amazon.s3.AmazonS3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
